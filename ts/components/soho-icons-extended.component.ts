1ed4a2a2f60620e1e96dacbd7d2a18b0679439b8
ppatton <phillip.patton@infor.com>
2016-06-15 14:30:12 -0500

    initial commit

---
+++ ts/components/soho-icons-extended.component.ts
@@ -0,0 +1,9 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector    : 'soho-icons-ext',
+    templateUrl : '/node_modules/@infor/sohoxi/dist/svg-extended.html'
+})
+
+export class SohoIconsExtended {}
---
+++ ts/components/soho-icons.component.ts
@@ -0,0 +1,9 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector    : 'soho-icons',
+    templateUrl : '/node_modules/@infor/sohoxi/dist/svg.html'
+})
+
+export class SohoIcons {}
