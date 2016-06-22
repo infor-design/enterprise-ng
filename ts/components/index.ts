a12c0b2ae476c0e1241407367d79c8e29e4eb9c6
ppatton <phillip.patton@infor.com>
2016-06-22 15:22:15 -0500

    changed accordian to accordion

--- ts/components/index.ts
+++ ts/components/index.ts
@@ -8,6 +8,6 @@ export * from './soho-icons.component';
 export * from './soho-icons-extended.component';
 export * from './soho-toolbar.component';
 export * from './soho-tabs.component';
-export * from './soho-accordian.component';
+export * from './soho-accordion.component';
 export * from './soho-dropdown.component';
 export * from './soho-expandablearea.component';
