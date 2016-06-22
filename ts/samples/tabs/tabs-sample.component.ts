8d1356067f9c90e6d47512288977639d04f3cd08
ppatton <phillip.patton@infor.com>
2016-06-22 09:34:36 -0500

    Added tabs component and sample

--- ts/samples/tabs/tabs-sample.component.ts
+++ ts/samples/tabs/tabs-sample.component.ts
@@ -2,14 +2,32 @@
 import {Component} from '@angular/core';
 
 import {SohoTabsComponent} from '../../components/index';
+import {SohoTabSelectedEvent} from "../../components/soho-tabs.component";
 
 @Component({
     selector : 'tabs-sample-component',
     directives : [SohoTabsComponent],
     template : `
-        <p>TabsSampleComponent</p>
-        <soho-tabs-component></soho-tabs-component>
+        <soho-tabs-component (tabSelected)="onTabSelected($event)"> 
+        
+            <ul class="tab-list">
+                <li><a href="#One">One</a></li>
+                <li><a href="#Two">Two</a></li>
+                <li><a href="#Three">Three</a></li>
+            </ul>
+            
+            <div class="tab-content">
+                <div id="One">One Content</div>
+                <div id="Two">Two Content</div>
+                <div id="Three">Three Content</div>
+            </div>
+        
+        </soho-tabs-component>
     `
 })
+export class TabsSampleComponent {
 
-export class TabsSampleComponent{}
+    private onTabSelected(event:SohoTabSelectedEvent) {
+        console.log("TabsSampleComponent.onTabSelected: " + JSON.stringify(event));
+    }
+}
