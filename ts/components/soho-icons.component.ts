f2775f0067e5a6a0a1c28a37fc14bf02a08a4949
tmcconechy <tim.mcconechy@infor.com>
2016-06-27 15:31:01 -0400

    - Cleaning out the repo

--- ts/components/soho-icons.component.ts
+++
@@ -1,9 +0,0 @@
-
-import {Component} from '@angular/core';
-
-@Component({
-    selector    : 'soho-icons',
-    templateUrl : '/node_modules/@infor/sohoxi/dist/svg.html'
-})
-
-export class SohoIcons {}
--- ts/samples/toolbar/main.ts
+++
@@ -1,7 +0,0 @@
-///<reference path="../../typings/tsd.d.ts" />
-
-// angular imports
-import {bootstrap} from "@angular/platform-browser-dynamic";
-import {AppComponent} from "./app.component";
-
-bootstrap(<any>AppComponent);
