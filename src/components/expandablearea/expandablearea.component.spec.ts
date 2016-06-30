f17cee240a1011e595bbf8b954584f3d29e1f72d
Tyler Wallace <tyler.wallace@infor.com>
2016-06-30 09:06:12 -0700

    Fix ElementRef in constructor error.

--- src/components/expandablearea/expandablearea.component.spec.ts
+++ src/components/expandablearea/expandablearea.component.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, ElementRef } from '@angular/core';
 
 import {
   beforeEach, beforeEachProviders,
@@ -13,8 +13,14 @@ import {
 import { ExpandableAreaComponent } from './expandablearea.component';
 
 describe('Component: Expandablearea', () => {
+  let elementRef: ElementRef;
+
+  beforeEach(inject([ElementRef], function (eref: ElementRef) {
+    elementRef = eref;
+  }));
+
   it('should create an instance', () => {
-    let component = new ExpandableAreaComponent();
+    let component = new ExpandableAreaComponent(elementRef);
     expect(component).toBeTruthy();
   });
 });
