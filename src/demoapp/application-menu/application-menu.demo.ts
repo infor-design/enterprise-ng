33bbe1631164970417273b8d6bb7c61dde169de1
Kris Hollenbeck <kris.hollenbeck@infor.com>
2016-07-08 11:08:38 -0500

    Merge branch 'master' of http://git.infor.com/scm/~khollenbeck/angular-components
    # Conflicts:
    #	src/components/index.ts
    #	src/demoapp/app.routes.ts
    #	src/demoapp/application-menu/application-menu.demo.html
    #	src/system-config.ts

--- src/demoapp/application-menu/application-menu.demo.ts
+++ src/demoapp/application-menu/application-menu.demo.ts
@@ -2,16 +2,13 @@ import { Component, HostBinding } from "@angular/core";
 import { ROUTER_DIRECTIVES } from "@angular/router";
 
 @Component({
+  moduleId: module.id,
   selector: 'soho-application-menu',
   directives: [ROUTER_DIRECTIVES],
-  templateUrl: '/demoapp/application-menu/application-menu.demo.html'
+  templateUrl: 'application-menu.demo.html'
 })
 export class ApplicationMenuDemo
 {
-  @HostBinding('id') get id() {
-    return 'application-menu';
-  }
-
   @HostBinding('class') get classes() {
     return 'application-menu is-open no-transition';
   }
