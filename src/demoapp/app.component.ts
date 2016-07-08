ecca753c7f3479180215fa4f148fd6b1bec59a23
Kris Hollenbeck <kris.hollenbeck@infor.com>
2016-07-08 11:16:28 -0500

    Merge branch 'master' of http://git.infor.com/scm/~khollenbeck/angular-components
    # Conflicts:
    #	src/components/index.ts
    #	src/demoapp/app.routes.ts
    #	src/demoapp/application-menu/application-menu.demo.html
    #	src/system-config.ts

--- src/demoapp/app.component.ts
+++ src/demoapp/app.component.ts
@@ -1,9 +1,14 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, AfterViewInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { SoHoIcon, SoHoIconExtended } from '../components';
+
 import { MastheadDemo } from './masthead/masthead.demo';
 import { HeaderDemo } from './header/header.demo';
 import { ApplicationMenuDemo } from './application-menu/application-menu.demo';
+
+import { MastheadComponent } from '../components';
+import { HeaderComponent } from '../components';
+import { ApplicationMenuComponent } from '../components';
 
 @Component({
   moduleId: module.id,
@@ -11,13 +16,21 @@ import { ApplicationMenuDemo } from './application-menu/application-menu.demo';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   directives : [
-    SoHoIcon, SoHoIconExtended, MastheadDemo, ApplicationMenuDemo, HeaderDemo, ROUTER_DIRECTIVES
+    SoHoIcon, SoHoIconExtended, MastheadDemo, HeaderDemo, ApplicationMenuDemo, ROUTER_DIRECTIVES
   ],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'SoHo Xi Controls in Angular 2!';
 
   @HostBinding('class') get classes() {
     return 'no-scroll';
+  }
+
+  ngAfterViewInit() {
+    let $toolbarElement:any = $('.toolbar');
+    $toolbarElement.toolbar();
+
+    let $applicationMenuElement:any = $('.application-menu');
+    $applicationMenuElement.applicationmenu({triggers: [$('.application-menu-trigger')]});
   }
 }
