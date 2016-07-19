287e9b8adce137b9618bf561d28ff96bd3919839
Theo Harper <Theo.Harper@infor.com>
2016-07-19 17:21:09 +0100

    Fixed toolbar which has fixed personalization.

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -2,34 +2,58 @@ import {
   Component,
   AfterViewInit,
   HostBinding,
-  ElementRef
+  ElementRef,
+  Output,
+  EventEmitter
 } from '@angular/core';
 
-import {
-  SoHoButtonComponent
-} from '../../components';
+declare var $: any;
 
 @Component({
   moduleId: module.id,
   selector: 'soho-header',
   templateUrl: 'header.component.html',
-  directives: [ SoHoButtonComponent ]
 })
 export class HeaderComponent implements AfterViewInit {
   @HostBinding('class') get classes() {
     return 'header is-personalizable';
   }
 
-  constructor(private elementRef: ElementRef) {}
+  // Reference to the jQuery element.
+  private jQueryElement: any;
+
+  // Reference to the annotated SoHoXi control
+  private header: any;
+
+  // This event is fired when the status of the header is changed.
+  @Output() onChange = new EventEmitter<any>();
+
+  // @Input() set updated(value : any) {
+  //   if (value) {
+  //     $(this.elementRef.nativeElement).trigger('updated');
+  //   }
+  // }
+
+  update() {
+    this.header.updated();
+  }
+
+  constructor(private elementRef: ElementRef) {
+  }
 
   ngAfterViewInit() {
-    // ngAfterViewInit lifecycle event - called after Angular creates the component's view(s).
-    // meaning the content is in the DOM and it's ok to run jQuery against it
+    // Wrap for later.
+    this.jQueryElement = $(this.elementRef.nativeElement);
 
-    let $toolbarElement: any = jQuery('.toolbar');
-    $toolbarElement.toolbar();
+    // Initialise the SoHoXi control.
+    this.jQueryElement.header();
 
-    let $applicationMenuElement: any = $('.application-menu');
-    $applicationMenuElement.applicationmenu({triggers: [jQuery('.application-menu-trigger')]});
+    // Once the control is initialised, extract the control
+    // plug-in from the element.  The element name is
+    // defined by the plug-in, but in this case is 'sohoxiHeader'.
+    this.header = this.jQueryElement.data('header');
+
+    // Initialize any event handlers.
+    this.jQueryElement.on('updated', (e: any, args: any) => { this.onChange.next(args); });
   }
 }
