9543310845d0a3793dfde674f7495181268c1e35
ppatton <phillip.patton@infor.com>
2016-08-11 14:59:38 -0500

    Refactor toolbar to better match the other soho-angular-components

--- src/demoapp/app.routes.ts
+++ src/demoapp/app.routes.ts
@@ -10,10 +10,12 @@ import { MenuButtonDemoComponent } from './menu-button/menu-button.demo';
 import { TreeServiceDemoComponent } from './tree/tree-service.demo';
 import { TreeContentDemoComponent } from './tree/tree-content.demo';
 import { TreeDynamicDemoComponent } from './tree/tree-dynamic.demo';
+
 import { DataGridServiceDemoComponent } from './datagrid/datagrid-service.demo';
 import { DataGridContentDemoComponent } from './datagrid/datagrid-content.demo';
 import { DataGridDynamicDemoComponent } from './datagrid/datagrid-dynamic.demo';
 import { DataGridBreadcrumbDemoComponent } from './datagrid/datagrid-breadcrumb.demo';
+
 import { DropdownDemoComponent } from './dropdown/dropdown.demo';
 import { DropdownMultiselectDemoComponent } from './dropdown/dropdown-multiselect.demo';
 import { SplitterDemoComponent } from './splitter/splitter.demo';
@@ -29,38 +31,37 @@ import { TabsDismissibleDemoComponent } from './tabs/tabs-dismissible.demo';
 import { TabsDropdownDemoComponent } from './tabs/tabs-dropdown.demo';
 import { TabsDataDrivenDemoComponent } from './tabs/tabs-datadriven.demo';
 import { TabsDynamicDemoComponent } from './tabs/tabs-dynamic.demo';
-import { ToolbarAllIconsDemoComponent } from './toolbar/toolbar-all-icons.demo';
 
 export const routes: RouterConfig = [
-  { path: '',                 component: ExpandableAreaDemoComponent }, // default
-  { path: 'button',           component: ButtonDemoComponent },
-  { path: 'busyindicator',    component: BusyIndicatorDemoComponent },
-  { path: 'dropdown',         component: DropdownDemoComponent },
-  { path: 'dropdown-multi',   component: DropdownMultiselectDemoComponent },
-  { path: 'expandablearea',   component: ExpandableAreaDemoComponent },
-  { path: 'toast',            component: ToastDemoComponent },
-  { path: 'icon',             component: IconDemoComponent },
-  { path: 'lookup',           component: LookupDemoComponent },
-  { path: 'mask',             component: MaskDemoComponent },
-  { path: 'menu-button',      component: MenuButtonDemoComponent },
+  { path: '',                    component: ExpandableAreaDemoComponent }, // default
+  { path: 'button',              component: ButtonDemoComponent },
+  { path: 'busyindicator',       component: BusyIndicatorDemoComponent },
+  { path: 'dropdown',            component: DropdownDemoComponent },
+  { path: 'dropdown-multi',      component: DropdownMultiselectDemoComponent },
+  { path: 'expandablearea',      component: ExpandableAreaDemoComponent },
+  { path: 'toast',               component: ToastDemoComponent },
+  { path: 'icon',                component: IconDemoComponent },
+  { path: 'lookup',              component: LookupDemoComponent },
+  { path: 'mask',                component: MaskDemoComponent },
+  { path: 'menu-button',         component: MenuButtonDemoComponent },
   { path: 'splitter',            component: SplitterDemoComponent },
-  { path: 'tree-dynamic',     component: TreeDynamicDemoComponent },
-  { path: 'tree-service',     component: TreeServiceDemoComponent },
-  { path: 'tree-content',     component: TreeContentDemoComponent },
-  { path: 'datagrid-dynamic', component: DataGridDynamicDemoComponent },
-  { path: 'datagrid-service', component: DataGridServiceDemoComponent },
-  { path: 'datagrid-content', component: DataGridContentDemoComponent },
+  { path: 'tree-dynamic',        component: TreeDynamicDemoComponent },
+  { path: 'tree-service',        component: TreeServiceDemoComponent },
+  { path: 'tree-content',        component: TreeContentDemoComponent },
+  { path: 'datagrid-dynamic',    component: DataGridDynamicDemoComponent },
+  { path: 'datagrid-service',    component: DataGridServiceDemoComponent },
+  { path: 'datagrid-content',    component: DataGridContentDemoComponent },
   { path: 'datagrid-breadcrumb', component: DataGridBreadcrumbDemoComponent },
-  { path: 'tabs-basic',       component: TabsBasicDemoComponent },
-  { path: 'tabs-vertical',    component: TabsVerticalDemoComponent },
-  { path: 'tabs-counts',      component: TabsCountsDemoComponent },
-  { path: 'tabs-dismissible', component: TabsDismissibleDemoComponent },
-  { path: 'tabs-dropdown',    component: TabsDropdownDemoComponent },
-  { path: 'tabs-datadriven',  component: TabsDataDrivenDemoComponent },
-  { path: 'tabs-dynamic',     component: TabsDynamicDemoComponent },
-  { path: 'toolbar-basic',    component: ToolbarBasicDemoComponent },
-  { path: 'toolbar-datadriven', component: ToolbarDataDrivenDemoComponent },
-  { path: 'toolbar-all-icons',  component: ToolbarAllIconsDemoComponent },
+  { path: 'tabs-basic',          component: TabsBasicDemoComponent },
+  { path: 'tabs-vertical',       component: TabsVerticalDemoComponent },
+  { path: 'tabs-counts',         component: TabsCountsDemoComponent },
+  { path: 'tabs-dismissible',    component: TabsDismissibleDemoComponent },
+  { path: 'tabs-dropdown',       component: TabsDropdownDemoComponent },
+  { path: 'tabs-datadriven',     component: TabsDataDrivenDemoComponent },
+  { path: 'tabs-dynamic',        component: TabsDynamicDemoComponent },
+  { path: 'toolbar-basic',       component: ToolbarBasicDemoComponent },
+  { path: 'toolbar-datadriven',  component: ToolbarDataDrivenDemoComponent },
+  { path: 'toolbar-all-icons',   component: ToolbarAllIconsDemoComponent },
   // { path: 'accordian',       component: AccordionSampleComponent},
 ];
 
