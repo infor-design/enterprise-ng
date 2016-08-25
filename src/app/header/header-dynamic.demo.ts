6f51b1b6c07a32279d72b3bcaf15607b3f6eadd3
ppatton <phillip.patton@infor.com>
2016-08-25 14:06:15 -0500

    Updated dynamic header to handle both header toolbar and header tabs

--- src/app/header/header-dynamic.demo.ts
+++ src/app/header/header-dynamic.demo.ts
@@ -4,8 +4,11 @@ import {
   Input,
   ViewChild
 } from '@angular/core';
-import { SohoToolbarOptions } from './header-dynamic-demo.model';
-import { SohoHeaderDynamicDemoRefService } from './header-dynamic-demo-ref.service';
+import { HeaderDynamicToolbarOptions, HeaderDynamicTabsetOptions } from './header-dynamic-demo.model';
+import { HeaderDynamicToolbarOptions, HeaderDynamicTabsetOptions } from './header-dynamic-demo.model';
+import { HeaderDynamicDemoRefService } from './header-dynamic-demo-ref.service';
+import { SohoToolbarComponent } from '../../components/toolbar/toolbar.component';
+import { SohoTabsComponent } from '../../components/tabs/tabs.component';
 
 @Component({
   selector: 'soho-header-dynamic-demo',
@@ -15,6 +18,7 @@ export class SohoHeaderDynamicDemoComponent {
   @HostBinding('class.header') get isHeader() { return true; };
   @HostBinding('class.is-personalizable') get isPersonalizable() { return true; };
   @HostBinding('class.has-toolbar') get hasHeaderToolbar() { return true; };
+  @HostBinding('class.has-tabs') get hasHeaderTabs() { return !!this.currentTabsOptions; };
 
   /**
    * Had to use a template variable to get the selector to work.
@@ -24,7 +28,15 @@ export class SohoHeaderDynamicDemoComponent {
    *   By element: @ViewChild('soho-toolbar')
    * Neither of these are working. investigate.
    */
-  @ViewChild('appDynamicHeaderToolbar') sohoToolbarComponent: any;
+  @ViewChild(SohoToolbarComponent) sohoToolbarComponent: SohoToolbarComponent;
+
+  /**
+   * Had to use a template variable to get the selector to work.
+   * TODO ppatton Neither of these are working. investigate.
+   *   By Type: @ViewChild(SohoToolbarComponent)   -
+   *   By element: @ViewChild('soho-toolbar')
+   */
+  @ViewChild(SohoTabsComponent) sohoTabsComponent: SohoTabsComponent;
 
   /**
    * The current screen title
@@ -32,30 +44,58 @@ export class SohoHeaderDynamicDemoComponent {
   @Input() sectionTitle: string;
 
   /**
-   * Set the toolbar using TabOptions
+   * Sets the header toolbar using TabOptions
    */
-  @Input() set toolbarOptions(options: SohoToolbarOptions) {
+  @Input() set toolbarOptions(options: HeaderDynamicToolbarOptions) {
     this.currentToolbarOptions = options;
-    if (this.sohoToolbarComponent) {
-      setTimeout(() => this.sohoToolbarComponent.updated());
-    }
+    setTimeout(() => {
+      if (this.sohoToolbarComponent) {
+        this.sohoToolbarComponent.updated();
+      }
+    });
   }
+
+  /**
+   * Sets the header tabset using TabOptions
+   */
+  @Input() public set tabOptions(options: HeaderDynamicTabsetOptions) {
+    this.currentTabsOptions = options;
+    setTimeout(() => {
+      if (this.sohoTabsComponent) {
+        this.sohoTabsComponent.updated();
+      }
+    });
+  }
+
+  /**
+   * Used by the component template to decide when to display the tabs.
+   * @type {any}
+   */
+  private currentToolbarOptions: HeaderDynamicToolbarOptions = null;
 
   /**
    * Used by the component template to decide when to display the toolbar.
    * @type {any}
    */
-  private currentToolbarOptions: SohoToolbarOptions = null;
+  private currentTabsOptions: HeaderDynamicTabsetOptions = null;
 
-  constructor(private headerRef: SohoHeaderDynamicDemoRefService) {
+  constructor(private headerRef: HeaderDynamicDemoRefService) {
     this.headerRef.instance = this;
   }
 
   /**
    * Get the current toolbar options.
-   * @returns {SohoToolbarOptions}
+   * @returns {HeaderDynamicToolbarOptions}
    */
-  get toolbarOptions(): SohoToolbarOptions {
+  get toolbarOptions(): HeaderDynamicToolbarOptions {
     return this.currentToolbarOptions;
+  }
+
+  /**
+   * Get the current toolbar options.
+   * @returns {HeaderDynamicToolbarOptions}
+   */
+  public get tabOptions(): HeaderDynamicTabsetOptions {
+    return this.currentTabsOptions;
   }
 }
