5c3ad3dbbd31d8834e06b3a0a43a113194670b33
Theo Harper <Theo.Harper@infor.com>
2016-08-30 16:54:37 +0100

    Made DataGrid an NgModule. Filled in selected row event interfaces. Added additional Api methods from jQuery control.

---
+++ src/components/splitter/splitter.component.ts
@@ -0,0 +1,72 @@
+﻿import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  OnDestroy,
+  ChangeDetectionStrategy
+} from '@angular/core';
+
+@Component({
+  selector: 'soho-splitter-left-pane',
+  template: `<ng-content></ng-content>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SohoSplitterLeftPaneComponent {
+  @HostBinding('class.pane-left') paneLeft= true;
+}
+
+@Component({
+  selector: 'soho-splitter-right-pane',
+  template: `<ng-content></ng-content>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SohoSplitterRightPaneComponent {
+  @HostBinding('class.pane-right') paneRight = true;
+}
+
+@Component({
+  moduleId: module.id,
+  selector: '[soho-splitter]',
+  templateUrl: 'splitter.component.html'
+})
+export class SohoSplitterComponent implements AfterViewInit, OnDestroy {
+  private jQueryElement: any;
+  private splitter: any;
+
+  constructor(private element: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    this.jQueryElement = jquery(this.element.nativeElement);
+
+    const options: any = {
+      axis: 'x',
+      resize: 'immediate', // (or end)
+      containment: null
+    };
+
+    this.jQueryElement.splitter(options);
+
+    this.splitter = this.jQueryElement.data('data');
+
+  // this.jQueryElement.on('split', (f) => this.onSplit(f));
+  }
+
+  ngOnDestroy() {
+    // Necessary clean up step (add additional here)
+    if (this.splitter) {
+      this.splitter.destroy();
+      this.splitter = null;
+    }
+  }
+}
+
+/**
+ * Holds all components usable for splitter
+ */
+export const SPLITTER_COMPONENTS = [
+  SohoSplitterLeftPaneComponent,
+  SohoSplitterRightPaneComponent,
+  SohoSplitterComponent
+];
