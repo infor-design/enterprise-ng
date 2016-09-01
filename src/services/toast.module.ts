2385c5cbc662e803fa14457584b69c0ffff6ef25
ppatton <phillip.patton@infor.com>
2016-09-01 08:39:24 -0500

    Fix for service module definition

--- src/services/toast.module.ts
+++ src/services/toast.module.ts
@@ -6,10 +6,7 @@ import { SohoToastService } from './toast.service';
   imports: [
     CommonModule
   ],
-  declarations: [
-    SohoToastService
-  ],
-  exports: [
+  providers: [
     SohoToastService
   ]
 })
