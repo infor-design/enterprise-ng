29596bd0c8d4fb22f8ac0c884d6debb4215921a7
mfitzgerald1 <michael.fitzgerald@infor.com>
2016-09-30 14:03:42 -0500

    SOHO-4567: SohoTimePicker - Refactor for enable, disable, readonly

--- src/app/timepicker/timepicker.demo.ts
+++ src/app/timepicker/timepicker.demo.ts
@@ -1,7 +1,12 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  ViewChild
 } from '@angular/core';
+
+import {
+  SohoTimePickerComponent
+} from '../../soho/timepicker/soho-timepicker.component';
 
 @Component({
   selector: 'soho-timepicker-demo',
@@ -9,18 +14,41 @@ import {
 })
 export class TimePickerDemoComponent implements OnInit {
 
+  @ViewChild(SohoTimePickerComponent) timepicker: SohoTimePickerComponent;
+
   private model = { // tslint:disable-line
     hhmm: '1:23 PM',
     HHmm24: ''
   };
   private showModel = false;
+  private timepickerDisabled = false;
+  private timepickerReadOnly = false;
 
   constructor() { }
+
   ngOnInit() { }
+
   toggleModel() {
     this.showModel = !this.showModel;
   }
+
   onChange(event: Event) {
     console.log('TimepickerDemoComponent.onChange');
+  }
+
+  setEnable() {
+    this.timepicker.disabled = false;
+    this.timepickerDisabled = this.timepicker.disabled;
+    this.timepickerReadOnly = this.timepicker.readonly;
+  }
+
+  setDisable() {
+    this.timepicker.disabled = true;
+    this.timepickerDisabled = this.timepicker.disabled;
+  }
+
+  setReadonly() {
+    this.timepicker.readonly = true;
+    this.timepickerReadOnly = this.timepicker.readonly;
   }
 }
