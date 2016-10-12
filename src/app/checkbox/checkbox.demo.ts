de0f92a4130dd0ad5c4d0903d6145476f5e5a667
vwiny <vonny.winy@infor.com>
2016-10-12 12:45:31 -0500

    update label wrapper to include class binding

--- src/app/checkbox/checkbox.demo.ts
+++ src/app/checkbox/checkbox.demo.ts
@@ -9,6 +9,20 @@ import {
 })
 export class CheckBoxDemoComponent implements OnInit {
 
+  private model = { // tslint:disable-line
+    checkBox1Value: false,
+    checkBox2Value: true,
+    checkBox3Value: false,
+    checkBox4Value: true,
+    checkBox5Value: true,
+  };
+
+  private id1 : string = "checkbox1";
+  private id2 : string = "checkbox2";
+  private id3 : string = "checkbox3";
+  private id4 : string = "checkbox4";
+  private id5 : string = "checkbox5";
+
   constructor() { }
   ngOnInit() { }
 
