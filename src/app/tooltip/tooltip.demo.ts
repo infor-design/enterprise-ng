2ded9c2121ca9b37e87d225d35fb52bfd1c38c8d
vwiny <vonny.winy@infor.com>
2016-10-18 16:22:44 -0500

    implement tooltip wrapper

--- src/app/tooltip/tooltip.demo.ts
+++ src/app/tooltip/tooltip.demo.ts
@@ -12,8 +12,8 @@ export class TooltipDemoComponent implements OnInit {
 
   @ViewChildren(SohoTooltipDirective) tooltips: QueryList<SohoTooltipDirective>;
 
-  private normalTooltipText: string = "Tooltips Provide<br> Additional Information";
-  private standardTooltipText: string = "<span style='text-align: left; display: inline-block;'><b style='text-transform: uppercase; line-height: 1.7em;'>Connected order</b><br>Tooltips Provide <br> Additional Information</span>";
+  private normalTooltipText: string = 'Tooltips Provide<br> Additional Information';
+  private standardTooltipText: string = '<span style=\'text-align: left; display: inline-block;\'><b style=\'text-transform: uppercase; line-height: 1.7em;\'>Connected order</b><br>Tooltips Provide <br> Additional Information</span>';
 
   constructor() { }
   ngOnInit() {
