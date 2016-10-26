782fe9fe566f8e8e1fa1a924841b216bf038d706
ppatton <phillip.patton@infor.com>
2016-10-26 10:45:20 -0500

    - added typings to tabs component

--- src/soho/soho.d.ts
+++ src/soho/soho.d.ts
@@ -2,17 +2,11 @@
 /// <reference path="./soho-components.d.ts" />
 // tslint:enable
 
-
-interface SohoTabsEvent extends JQueryEventObject {
-}
-
 /**
  * JQuery Integration
  */
-
 interface JQueryStatic {
 }
 
 interface JQuery {
-  tabs(): JQuery;
 }
