e697dbdec55d775cb00a504eb380106defca31e0
Theo Harper <Theo.Harper@infor.com>
2016-11-09 10:31:13 +0000

    Added Dialog Result Example

---
+++ src/soho/modal-dialog/soho-modal-dialog.spec.ts
@@ -0,0 +1,63 @@
+﻿// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// // import { By } from '@angular/platform-browser';
+// import { Component, DebugElement, ViewChild }    from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// import { SohoModalDialogModule } from './soho-modal-dialog.module';
+// import { SohoModalDialogService } from './soho-modal-dialog.service';
+
+
+// describe('Soho Modal Dialog Dropdown Render', () => {
+//   let dropdown:  SohoDataGridComponent;
+//   let component: SohoDataGridTestComponent;
+//   let fixture:   ComponentFixture<SohoDataGridTestComponent>;
+//   let de:        DebugElement;
+// //  let el:        HTMLElement;
+
+//   beforeEach( () => {
+//     TestBed.configureTestingModule({
+//       declarations: [ SohoDataGridTestComponent ],
+//       imports: [ FormsModule, SohoDataGridModule ]
+//     });
+
+//     fixture = TestBed.createComponent(SohoDataGridTestComponent);
+//     component = fixture.componentInstance;
+//     dropdown = component.dropdown;
+
+//     de = fixture.debugElement;
+//     // el = de.query(By.css('div[soho-datagrid]')).nativeElement;
+
+//     // fixture.detectChanges();
+//   });
+
+//   it('Check HTML content', () => {
+//     fixture.detectChanges();
+
+//     // expect(el.nodeName).toEqual('SELECT');
+//     // expect(el.id).toEqual(dropdown.id);
+//     // expect(el.classList).toContain('dropdown');
+//     // expect(el.hasAttribute('noSearch')).toBeTruthy('noSearch');
+
+//     // expect(el.childElementCount).toBe(9);
+
+//     // let i = 0;
+//     // component.options.forEach(option => {
+//     //   expect(el.children[i].nodeName).toBe('OPTION');
+//     //   expect(el.children[i].getAttribute('value')).toBe(option.value);
+//     //   expect(el.children[i++].innerHTML).toBe(option.label);
+//     // });
+
+//     // dropdown.noSearch = false;
+//     // fixture.detectChanges();
+
+//     // expect(el.hasAttribute('noSearch')).toBeTruthy('noSearch');
+//   });
+
+// });
+
+// @Component({
+//   template: ``
+// })
+// class SohoDataGridTestComponent {
+//   @ViewChild(SohoDataGridComponent) dropdown: SohoDataGridComponent;
+// }
