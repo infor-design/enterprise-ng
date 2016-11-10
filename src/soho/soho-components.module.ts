1e5cc0e0895ea8f7de5a30373a0e23987cd6504c
swolyniec <stanley.wolyniec@infor.com>
2016-11-10 15:00:11 -0600

    First cut of the step control angular component

--- src/soho/soho-components.module.ts
+++ src/soho/soho-components.module.ts
@@ -38,6 +38,7 @@ import { SohoToolbarModule } from './toolbar/soho-toolbar.module';
 import { SohoTooltipModule } from './tooltip/soho-tooltip.module';
 import { SohoTrackDirtyModule } from './trackdirty/soho-trackdirty.module';
 import { SohoTreeModule } from './tree/soho-tree.module';
+import { SohoStepControlModule } from './stepcontrol/soho-step-control.module';
 
 @NgModule({
   imports: [
