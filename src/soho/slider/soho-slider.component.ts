6d56accbead367e1d34ba2543f2c46f23c302753
aaralikar <akila.aralikar@infor.com>
2016-11-14 13:39:38 -0600

    Add Slider Component Unit tests - alphabetized imports

--- src/soho/slider/soho-slider.component.ts
+++ src/soho/slider/soho-slider.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterViewChecked,
   AfterViewInit,
   Component,
   ElementRef,
@@ -6,7 +7,7 @@ import {
   HostBinding,
   Input,
   OnDestroy,
-  Output, AfterViewChecked,
+  Output,
 } from '@angular/core';
 
 @Component({
