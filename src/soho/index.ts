e8026c652ec6e8862f586070e6c22c1362643c2a
pvyas <purva.vyas@infor.com>
2016-11-17 09:41:41 -0600

    spinbox

--- src/soho/index.ts
+++ src/soho/index.ts
@@ -25,6 +25,7 @@ export * from './splitter';
 export * from './slider';
 export * from './spinbox';
 export * from './stepprocess';
+export * from './spinbox';
 export * from './tabs';
 export * from './textarea';
 export * from './timepicker'
