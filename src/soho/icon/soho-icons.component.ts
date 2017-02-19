cddb1d7799ac778d937345f68753bc41ebab88e7
ppatton <phillip.patton@infor.com>
2017-02-19 12:27:01 -0600

    Fix for svg element taking height causing the masthead to be pushed down.

--- src/soho/icon/soho-icons.component.ts
+++ src/soho/icon/soho-icons.component.ts
@@ -1,8 +1,10 @@
 
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'soho-icons',
   templateUrl: '../../assets/svg/svg.html',
 })
-export class SohoIconsComponent {}
+export class SohoIconsComponent {
+  @HostBinding('style.display') none = 'none';
+}
