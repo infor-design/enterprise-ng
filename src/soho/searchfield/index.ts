0d19ed4baa457590a4e0763a5bd0bff5519d8987
ppatton <phillip.patton@infor.com>
2017-02-23 08:51:42 -0600

    Changes to toolbar search field Move the SohoToolbarSearchFieldComponent into soho-toolbar.cmponent.ts as it's really a part of a toolbar. Remove soho-toolbar-searchfield.component.ts Update header-dynamic to have a search field that is not collapsible and bind it's values to a variable Listen for events fired from the soho-toolbar-searchfield

--- src/soho/searchfield/index.ts
+++ src/soho/searchfield/index.ts
@@ -1,3 +1,2 @@
 export * from './soho-searchfield.component';
 export * from './soho-searchfield.module';
-export * from './soho-toolbar-searchfield.component';
--- src/soho/searchfield/soho-searchfield.module.ts
+++ src/soho/searchfield/soho-searchfield.module.ts
@@ -4,23 +4,15 @@ import {
   SohoSearchFieldComponent,
   SohoSearchFieldWrapperComponent
 } from './soho-searchfield.component';
-import {
-  SohoToolbarSearchFieldComponent,
-  SohoToolbarSearchFieldWrapperComponent
-} from './soho-toolbar-searchfield.component';
 
 @NgModule({
   declarations: [
     SohoSearchFieldComponent,
     SohoSearchFieldWrapperComponent,
-    SohoToolbarSearchFieldComponent,
-    SohoToolbarSearchFieldWrapperComponent
   ],
   exports: [
     SohoSearchFieldComponent,
     SohoSearchFieldWrapperComponent,
-    SohoToolbarSearchFieldComponent,
-    SohoToolbarSearchFieldWrapperComponent
   ],
 })
 export class SohoSearchFieldModule {}
