bfbde7f51c2af1a4b01abcae66bfcb18b8d2198f
Theo Harper <theo.harper@infor.com>
2017-02-27 14:28:55 +0000

    Revert "SwapList - first pass"
    This reverts commit b71ac72c9916039c3a47b3daa3452a658b2c98d8.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-﻿import { ModuleWithProviders } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AlertDemoComponent } from './alert/alert.demo';
@@ -50,7 +50,6 @@ import { SplitterVerticalDemoComponent } from './splitter/splitter-vertical.demo
 import { SpinboxDemoComponent } from './spinbox/spinbox.demo';
 import { StepProcessDemoComponent } from './stepprocess/stepprocess.demo';
 import { StepProcessDataDrivenDemoComponent } from './stepprocess/stepprocess-data-driven.demo';
-import { SwapListDemoComponent } from './swaplist/swaplist.demo';
 import { TabsBasicDemoComponent } from './tabs/tabs-basic.demo';
 import { TabsCountsDemoComponent } from './tabs/tabs-counts.demo';
 import { TabsDataDrivenDemoComponent } from './tabs/tabs-datadriven.demo';
@@ -130,7 +129,6 @@ export const routes: Routes = [
   { path: 'step-process',          component: StepProcessDemoComponent },
   { path: 'step-data-driven',      component: StepProcessDataDrivenDemoComponent },
   { path: 'step-process-vetoable', component: StepProcessVetoableDemoComponent },
-  { path: 'swaplist',			   component: SwapListDemoComponent },
   { path: 'spinbox',               component: SpinboxDemoComponent },
   { path: 'tabs-basic',            component: TabsBasicDemoComponent },
   { path: 'tabs-counts',           component: TabsCountsDemoComponent },
--- src/soho/swaplist/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './soho-swaplist.component';
-export * from './soho-swaplist.module';
