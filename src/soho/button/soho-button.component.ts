ccfd3bc57832ed05dafd710aff1c4101cac23da4
ppatton <phillip.patton@infor.com>
2017-05-10 08:42:39 -0500

    Added toggle button

--- src/soho/button/soho-button.component.ts
+++ src/soho/button/soho-button.component.ts
@@ -6,8 +6,10 @@ import {
   HostBinding,
   HostListener,
   Input,
-  OnDestroy
+  OnDestroy,
+  OnInit,
 } from '@angular/core';
+import { ArgumentHelper } from '@infor/sohoxi-angular';
 
 /**
  * Supported button types.
@@ -19,7 +21,7 @@ export type SohoButtonType = 'btn' | 'primary' | 'secondary' | 'tertiary' | 'ico
   templateUrl: './soho-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SohoButtonComponent implements AfterViewInit, OnDestroy {
+export class SohoButtonComponent implements AfterViewInit, OnDestroy, OnInit {
 
   // -------------------------------------------
   // Supported button types.
@@ -45,14 +47,54 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
   /** The type of the button. */
   private buttonType: SohoButtonType;
 
+  private _buttonOptions: SohoButtonOptions = {};
+  private _isToggle = false;
+  private _isPressed = false;
+
   /** The type of the button, defaulting to 'secondary'. */
   @Input('soho-button') set sohoButton(type: SohoButtonType) {
     this.buttonType = type ? type : SohoButtonComponent.SECONDARY;
+  }
 
-    if (type === SohoButtonComponent.FAVORITE) {
-      this.toggle = 'star-outlined';
-      this.icon = 'star-filled';
+  /**
+   * Sets the button options
+   */
+  @Input() set buttonOptions(buttonOptions: SohoButtonOptions) {
+    ArgumentHelper.checkNotNull('buttonOptions', buttonOptions);
+
+    this._buttonOptions = buttonOptions;
+    if (this.jQueryElement) {
+      // todo: how to update the button when options change?
     }
+  }
+  get buttonOptions(): SohoButtonOptions {
+    return this._buttonOptions;
+  }
+
+  @Input() set toggleOnIcon(toggleOnIcon: string) {
+    this._buttonOptions.toggleOnIcon = toggleOnIcon;
+    if (this.jQueryElement) {
+      // todo: how to update the button when toggleOnIcon change?
+    }
+  }
+
+  @Input() set toggleOffIcon(toggleOffIcon: string) {
+    this._buttonOptions.toggleOffIcon = toggleOffIcon;
+    if (this.jQueryElement) {
+      // todo: how to update the button when toggleOffIcon change?
+    }
+  }
+
+  /**
+   * Whether this button should be a toggle button or not. Alternate toggle on/off icons
+   * can be used through toggleOnIcon/toggleOffIcon inputs.
+   */
+  @Input() set isToggle(isToggle: boolean) {
+    this._isToggle = isToggle;
+  }
+
+  get isToggle(): boolean {
+    return this._isToggle;
   }
 
   /**
@@ -71,10 +113,15 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
    * Binary state (toggle):
    *  0 - shows toggle
    *  1 - shows icon (default)
+   *
+   *  @deprecated use isToggle=true input instead along with toggleOnIcon/toggleOffIcon options
    */
-  @Input() state = true;
+  @Input() state = undefined;
 
-  /** The icon to be used when the state is false. */
+  /**
+   * The icon to be used when the state is false.
+   * @deprecated use isToggle=true input instead along with toggleOnIcon/toggleOffIcon options
+   */
   @Input() toggle: string;
 
   @HostBinding('class.btn')
@@ -102,6 +149,10 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
     return this.buttonType === SohoButtonComponent.ICON || this.buttonType === SohoButtonComponent.FAVORITE;
   };
 
+  @HostBinding('class.btn-toggle') get btnToggle() {
+    return this.isToggle;
+  }
+
   @HostBinding('class.icon-favorite') get iconFavorite(): boolean {
     return this.buttonType === SohoButtonComponent.FAVORITE;
   };
@@ -115,8 +166,17 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
     return this.isSubmit ? 'submit' : 'button';
   };
 
+  /**
+   * @deprecated no longer needed once this.toggle is removed.
+   */
   @HostListener('click') toggleState() {
-    this.state = !this.state;
+    if (this.toggle) {
+      this.state = !this.state;
+    }
+  }
+
+  @HostBinding('attr.aria-pressed') get ariaPressed() {
+    return this.isPressed();
   }
 
   /**
@@ -131,12 +191,25 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
   // Lifecycle Events
   // ------------------------------------------
 
+  ngOnInit() {
+    if (this.buttonType === SohoButtonComponent.FAVORITE) {
+      if (this.isToggle) {
+        this.toggleOffIcon = 'star-outlined';
+        this.toggleOnIcon = 'star-filled';
+      } else {
+        // deprecated in 4.3.0 sohoxi
+        this.toggle = 'star-outlined';
+        this.icon = 'star-filled';
+      }
+    }
+  }
+
   ngAfterViewInit() {
     // Wrap the element in a jQuery selector.
     this.jQueryElement = jQuery(this.element.nativeElement);
 
     // Initialise the Soho control.
-    this.jQueryElement.button();
+    this.jQueryElement.button(this._buttonOptions);
 
     // Initialize title attribute as a soho tooltip
     if (this.jQueryElement.has('[title]')) {
@@ -148,12 +221,14 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
     // by the plug-in, but in this case is 'button'.
     this.button = this.jQueryElement.data('button');
 
-    // turn off the default handling of the favorite icon switching
-    // in the sohoxi controls (button.js). This is so that only this
-    // button-component handles the switching of the toggle icon for
-    // favorite.
-    if (this.buttonType === SohoButtonComponent.FAVORITE) {
-      this.jQueryElement.off('click.favorite');
+    if (this.state !== undefined) {
+      // turn off the default handling of the favorite icon switching
+      // in the sohoxi controls (button.js). This is so that only this
+      // button-component handles the switching of the toggle icon for
+      // favorite.
+      if (this.buttonType === SohoButtonComponent.FAVORITE) {
+        this.jQueryElement.off('click.favorite');
+      }
     }
 
     // There are no 'extra' event handler for button.
@@ -169,10 +244,25 @@ export class SohoButtonComponent implements AfterViewInit, OnDestroy {
     }
   }
 
+  get hasIcon() {
+    return (this.icon || (this.buttonOptions.toggleOnIcon && this.buttonOptions.toggleOffIcon));
+  }
+
   get currentIcon() {
-    if (!this.toggle) {
-      return this.icon;
+    if (this.isToggle && this.buttonOptions.toggleOnIcon && this.buttonOptions.toggleOffIcon) {
+      return this.isPressed() ? this.buttonOptions.toggleOnIcon : this.buttonOptions.toggleOffIcon;
     }
-    return this.state ? this.icon : this.toggle;
+
+    if (this.toggle) {
+      return this.state ? this.icon : this.toggle;
+    }
+
+    return this.icon;
+  }
+
+  public isPressed(): boolean {
+    const pressed = this.element.nativeElement.getAttribute('aria-pressed');
+    this._isPressed = (pressed === true || pressed === 'true');
+    return this._isPressed;
   }
 }
