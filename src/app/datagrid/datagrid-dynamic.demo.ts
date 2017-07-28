d0bb8f3820345427445f1e3f649d3c65d1450d51
ppatton <phillip.patton@infor.com>
2017-07-28 13:11:30 -0500

    Added functions to call on popup menu

--- src/app/datagrid/datagrid-dynamic.demo.ts
+++ src/app/datagrid/datagrid-dynamic.demo.ts
@@ -21,27 +21,23 @@ import {
   selector: 'soho-datagrid-dynamic-demo',
   templateUrl: './datagrid-dynamic.demo.html',
   providers: [DataGridDemoService],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DataGridDynamicDemoComponent implements AfterContentInit, AfterViewInit {
+export class DataGridDynamicDemoComponent implements AfterViewInit {
   @ViewChild(SohoDataGridComponent) dataGrid: SohoDataGridComponent;
   @ViewChild(SohoBusyIndicatorDirective) busyIndicator: SohoBusyIndicatorDirective;
-  // @ViewChild(SohoPopupMenuComponent) popupMenu: SohoPopupMenuComponent;
+  @ViewChild(SohoPopupMenuComponent) popupMenu: SohoPopupMenuComponent;
 
   private _subject$ = new BehaviorSubject([]);
-
   public data = this._subject$.asObservable();
-
   public displayContextMenu = false;
   public contextMenuEvent: any;
   public contextMenuId = 'grid-context-menu';
+  private menuItemsChoice = -1;
+  public menuItems: Array<{label: string}> = [];
 
   constructor(
     private service: DataGridDemoService
   ) {}
-
-  ngAfterContentInit() {
-  }
 
   ngAfterViewInit() {
     setTimeout(() => this.addRows(), 1000);
@@ -85,37 +81,109 @@ export class DataGridDynamicDemoComponent implements AfterContentInit, AfterView
   }
 
   onSelected(e: SohoDataGridSelectedEvent) {
-    console.log('onSelected() - rows ' + e.rows)
+    console.log('onSelected()', e)
   }
 
   onRowDoubleClicked(e: SohoDataGridRowClicked) {
-    console.log('onRowDoubleClicked() - row: ' + e.row + ', cell: ' + e.cell);
+    console.log('onRowDoubleClicked()', e);
   }
 
   onRowClicked(e: SohoDataGridRowClicked) {
-    console.log('onRowClicked() - row: ' + e.row + ', cell: ' + e.cell);
+    console.log('onRowClicked()', e);
   }
 
   onContextMenu(e: SohoDataGridRowClicked) {
     console.log('contextmenu fired', e);
-    this.contextMenuEvent = e.originalEvent;
-    this.displayContextMenu = true;
-  }
 
-  onMenuItemSelected(e: any) {
-    console.log('onMenuItemSelected()- ' + e);
-  }
+    // -----------------------------------------------------------------
+    // destroy any existing popup menu so that the dom markup is
+    // completely removed. this is needed before the new menu is
+    // built and displayed.
+    // -----------------------------------------------------------------
+    if (this.popupMenu) {
+      this.contextMenuEvent = null;
+      this.displayContextMenu = false;
 
-  onBeforeContextMenuOpen(e: any) {
-    console.log('onBeforeContextMenuOpen()- ' + e);
+      this.popupMenu.close();
+      this.popupMenu.teardown();
+    }
+
+    // -----------------------------------------------------------------
+    // select right click row, unselect any other row first.
+    // -----------------------------------------------------------------
+    this.dataGrid.unSelectAllRows();
+    this.dataGrid.selectRow(e.row);
+
+    // -----------------------------------------------------------------
+    // build the right click menu items array dynamically. When the
+    // context menu is generated into the dom it will use these items
+    // -----------------------------------------------------------------
+    this.buildMenuOptions();
+
+    // -----------------------------------------------------------------
+    // cause the new right click menu markup to be built, which
+    // causes the popup menu to be displayed.
+    // -----------------------------------------------------------------
+    setTimeout(() => {
+      this.contextMenuEvent = e.originalEvent;
+      this.displayContextMenu = true;
+    });
   }
 
   onContextMenuClose(e: any) {
-    console.log('onContextMenuClose()- ' + e);
+    console.log('onContextMenuClose()', e);
+
+    this.contextMenuEvent = null;
     this.displayContextMenu = false;
   }
 
+  onMenuItemSelected(e: any) {
+    console.log('onMenuItemSelected()', e);
+  }
+
+  onBeforeContextMenuOpen(e: any) {
+    console.log('onBeforeContextMenuOpen()', e);
+  }
+
   onContextMenuOpen(e: any) {
-    console.log('onContextMenuOpen()- ' + e);
+    console.log('onContextMenuOpen()', e);
+  }
+
+  private buildMenuOptions() {
+    if (this.menuItemsChoice === 2) {
+      this.menuItemsChoice = -1;
+    }
+
+    this.menuItemsChoice++;
+    this.menuItems = []; // reset menuItems array.
+
+    switch (this.menuItemsChoice) {
+      case 0:
+        this.menuItems.push({ label: 'Setting One' });
+        this.menuItems.push({ label: 'Setting Two' });
+        this.menuItems.push({ label: 'Settings Three' });
+        this.menuItems.push({ label: 'Setting Four' });
+        this.menuItems.push({ label: 'Setting Five' });
+        break;
+
+      case 1:
+        this.menuItems.push({ label: 'Option One' });
+        this.menuItems.push({ label: 'Option Two' });
+        this.menuItems.push({ label: 'Option Three' });
+        this.menuItems.push({ label: 'Option Four' });
+        this.menuItems.push({ label: 'Option Five' });
+        this.menuItems.push({ label: 'Option Six' });
+        this.menuItems.push({ label: 'Option Seven' });
+        this.menuItems.push({ label: 'Option Eight' });
+        this.menuItems.push({ label: 'Option Nine' });
+        this.menuItems.push({ label: 'Option Ten' });
+        break;
+
+      case 2:
+        this.menuItems.push({ label: 'Action One' });
+        this.menuItems.push({ label: 'Action Two' });
+        this.menuItems.push({ label: 'Action Three' });
+        break;
+    }
   }
 }
