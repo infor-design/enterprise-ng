07a64674adf0da3423484db311e41fe09f69b9a2
Ed Coyle <Ed.Coyle@infor.com>
2017-10-09 09:05:48 -0400

    - Fixed call to `super`

--- src/soho/utils/deprecated-event-emitter.ts
+++ src/soho/utils/deprecated-event-emitter.ts
@@ -8,8 +8,9 @@ export class DeprecatedEventEmitter<T> extends EventEmitter<T> {
     super();
   }
 
-  subscribe(...args) {
+  subscribe(generatorOrNext?: any, error?: any, complete?: any) {
     console.warn(`"${this.deprecatedName}" event has been deprecated in favor of "${this.newName}" and will be removed in the next release.`);
+    super.subscribe(generatorOrNext, error, complete);
   }
 
 }
