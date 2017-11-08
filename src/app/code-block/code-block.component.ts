830d9a33131edf4c424d357fa62d68214645159b
Tim McConechy <tim.mcconechy@infor.com>
2017-11-08 16:01:47 -0500

    - More padding fixes

--- src/app/code-block/code-block.component.ts
+++ src/app/code-block/code-block.component.ts
@@ -8,39 +8,42 @@ import {
 @Component({
   selector: '[lm-code-block]', // tslint:disable-line
   template: `<div class="code-block-container">
-      <div class="code-block">
-      <ng-content></ng-content></div>
-      <button type="button" class="btn-icon hide-focus">
-        <svg class="icon" focusable="false" aria-hidden="true" role="presentation">
-          <use xlink:href="#icon-launch"></use>
-        </svg>
-        <span>Launch</span>
-      </button>
+      <div class="code-block input-style">
+        <ng-content></ng-content>
+      </div>
+      <div class="code-block-buttons">
+        <button type="button" class="btn-icon hide-focus">
+          <svg class="icon" focusable="false" aria-hidden="true" role="presentation">
+            <use xlink:href="#icon-launch"></use>
+          </svg>
+          <span>Launch</span>
+        </button>
 
-      <button type="button" class="btn-actions code-block-actions btn-menu"
-        soho-context-menu trigger="click">
-        <svg class="icon" focusable="false" aria-hidden="true" role="presentation">
-          <use xlink:href="#icon-more"></use>
-        </svg>
-        <span>Launch</span>
-      </button>
-      <ul soho-popupmenu>
-        <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
-        <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
-        <li soho-popupmenu-item>
-          <a soho-popupmenu-label>Drill Around</a>
-          <ul soho-popupmenu>
-            <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
-            <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
-          </ul>
-        </li>
-        <li soho-popupmenu-separator singleSelectableSection="true"></li>
-        <li soho-popupmenu-heading>Options</li>
-        <li soho-popupmenu-item
-        (click)="toggleLabels()" [isChecked]="showLabels" isSelectable="true">
-            <a soho-popupmenu-label>Show Labels</a>
-        </li>
-      </ul>
+        <button type="button" class="btn-actions code-block-actions btn-menu"
+          soho-context-menu trigger="click">
+          <svg class="icon" focusable="false" aria-hidden="true" role="presentation">
+            <use xlink:href="#icon-more"></use>
+          </svg>
+          <span>Launch</span>
+        </button>
+        <ul soho-popupmenu>
+          <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
+          <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
+          <li soho-popupmenu-item>
+            <a soho-popupmenu-label>Drill Around</a>
+            <ul soho-popupmenu>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
+            </ul>
+          </li>
+          <li soho-popupmenu-separator singleSelectableSection="true"></li>
+          <li soho-popupmenu-heading>Options</li>
+          <li soho-popupmenu-item
+          (click)="toggleLabels()" [isChecked]="showLabels" isSelectable="true">
+              <a soho-popupmenu-label>Show Labels</a>
+          </li>
+        </ul>
+      </div>
     </div>`,
   styleUrls: ['./code-block.component.css']
 })
