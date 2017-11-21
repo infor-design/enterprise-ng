17abe49c8657d3f4d6422efcb6be37bd21b0273e
Tim McConechy <tim.mcconechy@infor.com>
2017-11-21 13:22:15 -0500

    - Fixed fileupload labels to make the examples more clear - Added more fixes to code block editor (Will come back to this later)

--- src/app/datagrid/datagrid-code-block-editor.demo.ts
+++ src/app/datagrid/datagrid-code-block-editor.demo.ts
@@ -23,25 +23,146 @@ import {
 } from '../code-block/code-block.component';
 
 @Component({
-    template: `<div lm-code-block [(ngModel)]="value"></div>`
-  //               soho-mask
-  //               alignRight="true"
-  //               [process]="'number'"
-  //               [integerLimit]="3"
-  //               (write)="onMaskWrite($event)"/>
+  //  template: `<div lm-code-block [(ngModel)]="value"></div>`
+  // TODO: Probably use an ngIf and insert these from data
+  template: `
+      <div lm-code-block ngDefaultControl>
+
+          <div class="field">
+            <label soho-label for="ledger">Ledger</label>
+            <input soho-lookup soho-field-options name="ledger" id="ledger" value="CORE" size="4"
+              [autoWidth]="true"
+              [(ngModel)]="model.ledger"
+              [columns]="columns"
+              [dataset]="ledgerData"
+              field="column1">
+
+            <button soho-button="icon" icon="more" class="btn-actions btn-menu" soho-context-menu trigger="click"></button>
+            <ul soho-popupmenu>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
+              <li soho-popupmenu-item>
+                <a soho-popupmenu-label>Drill Around</a>
+                <ul soho-popupmenu>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <div class="field">
+            <label soho-label for="accounting-entity">Accounting Entity</label>
+            <input soho-lookup soho-field-options name="accounting-entity" id="accounting-entity" type="text" value="1001" size="4"
+              [autoWidth]="true"
+              [(ngModel)]="model.accountingEntity"
+              [columns]="columns"
+              [dataset]="accountingData"
+              field="column1">
+
+            <button soho-button="icon" icon="more" class="btn-actions btn-menu" soho-context-menu trigger="click"></button>
+            <ul soho-popupmenu>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
+              <li soho-popupmenu-item>
+                <a soho-popupmenu-label>Drill Around</a>
+                <ul soho-popupmenu>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
+                </ul>
+              </li>
+              </ul>
+          </div>
+
+          <div class="field">
+            <label soho-label for="accounting-unit">Accounting Unit</label>
+            <input soho-lookup soho-field-options name="accounting-unit" id="accounting-unit" type="text" value="99 CORP" size="6"
+              [autoWidth]="true"
+              [(ngModel)]="model.accountingUnit"
+              [columns]="columns"
+              [dataset]="accountingData"
+              field="column1">
+
+            <button soho-button="icon" icon="more" class="btn-actions btn-menu" soho-context-menu trigger="click"></button>
+            <ul soho-popupmenu>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
+              <li soho-popupmenu-item>
+                <a soho-popupmenu-label>Drill Around</a>
+                <ul soho-popupmenu>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <div class="field">
+            <label soho-label for="cost-center">Cost Center</label>
+            <input soho-lookup soho-field-options name="cost-center" id="cost-center" type="text" value="102" size="3"
+              [autoWidth]="true"
+              [(ngModel)]="model.costCenter"
+              [columns]="columns"
+              [dataset]="costCenterData"
+              field="column1">
+
+            <button soho-button="icon" icon="more" class="btn-actions btn-menu" soho-context-menu trigger="click"></button>
+            <ul soho-popupmenu>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
+              <li soho-popupmenu-item>
+                <a soho-popupmenu-label>Drill Around</a>
+                <ul soho-popupmenu>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <div class="field">
+            <label soho-label for="department">Department</label>
+            <input soho-lookup soho-field-options name="department" id="department" type="text" value="102" size="3"
+              [autoWidth]="true"
+              [(ngModel)]="model.department"
+              [columns]="columns"
+              [dataset]="departmentData"
+              field="column1">
+
+            <button soho-button="icon" icon="more" class="btn-actions btn-menu" soho-context-menu trigger="click"></button>
+            <ul soho-popupmenu>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Field History</a></li>
+              <li soho-popupmenu-item><a soho-popupmenu-label>Show Pending Changes</a></li>
+              <li soho-popupmenu-item>
+                <a soho-popupmenu-label>Drill Around</a>
+                <ul soho-popupmenu>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 1</a></li>
+                  <li soho-popupmenu-item><a soho-popupmenu-label>Sub Menu 2</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+      </div>
+  `
 })
 export class CodeBlockEditorComponent  implements SohoDataGridCellEditor {
   @ViewChild(CodeBlockComponent) codeblock: CodeBlockComponent;
 
   value: string;
 
+  public model = {
+    ledger: 'CORE',
+    accountingEntity: '1001',
+    accountingUnit: '99 CORP',
+    costCenter: '102',
+    department: '102'
+  };
+
   constructor( @Inject('args') public args: SohoDataGridPostRenderCellArgs) {
     this.value = args.value;
+    console.log(args);
   }
-
-  // onMaskWrite(event: any) {
-  //   console.log(`CodeBlockEditorComponent ${this.args.row} onMaskWrite: ${event}`);
-  // }
 
   // @region Soho Editor Implementation
   val(value?: any) {
@@ -62,7 +183,8 @@ export const COLUMNS: SohoDataGridColumn[] = [
     name: 'Company',
     field: 'companyId',
     filterType: 'text',
-    width: 200
+    width: 200,
+    editor: Editors.Input
   },
 
   {
@@ -70,12 +192,13 @@ export const COLUMNS: SohoDataGridColumn[] = [
     name: 'Name',
     field: 'companyName',
     filterType: 'text',
-    width: 200
+    width: 200,
+    editor: Editors.Input
   },
 
   {
     id: 'code-block',
-    name: 'Code BlocK',
+    name: 'Code Block',
     field: 'code-block',
     sortable: false,
     filterType: 'text',
