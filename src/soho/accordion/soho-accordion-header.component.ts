d3d38255417dc028734197ccb139d5f82c918403
krishollenbeck <hollenbeck.kris@gmail.com>
2017-12-12 10:26:42 -0600

    Need to be able to set isExpanded via a host binding. Having aria-expanded hard coded to false in the template causes the panes to collapse on an update.

--- src/soho/accordion/soho-accordion-header.component.ts
+++ src/soho/accordion/soho-accordion-header.component.ts
@@ -26,6 +26,7 @@ export class SohoAccordionHeaderComponent implements AfterViewInit {
   @HostBinding('class.is-disabled') isDisabled = false;
   @HostBinding('style.display') get isBlockDisplay() { return 'block'; }
   @HostBinding('class.accordion-header') get isAccordionHeader() { return true; }
+  @HostBinding('class.is-expanded') @Input() isExpanded = false;
 
   /**
    * Constructor.
