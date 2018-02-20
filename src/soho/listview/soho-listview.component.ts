721ee971248c0faed262ece7772371c5ba84bdf5
ppatton <phillip.patton@infor.com>
2018-02-20 14:02:57 -0600

    cleanup

--- src/soho/listview/soho-listview.component.ts
+++ src/soho/listview/soho-listview.component.ts
@@ -129,7 +129,6 @@ export class SohoListViewMicroComponent {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SohoListViewComponent implements AfterViewInit, OnDestroy, AfterViewChecked {
-  @HostBinding('attr.d')
   @ContentChildren(SohoListViewItemComponent) items: QueryList<SohoListViewItemComponent>;
 
   /**
