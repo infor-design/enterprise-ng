586a7f98326e8c489cbc923ba31fd39aaacd18aa
Ed Coyle <Ed.Coyle@infor.com>
2018-03-06 17:00:41 -0500

    SOHO-7303
    - Changed Soho-Toolbar and Soho-Popupmenu components to allow settings to be propagated through their `updated` methods.

--- src/soho/popupmenu/soho-popupmenu.d.ts
+++ src/soho/popupmenu/soho-popupmenu.d.ts
@@ -69,7 +69,7 @@ interface SohoPopupMenuStatic {
   getSelected(): any;
 
   /** Updates the control to reflect the settings. */
-  updated(): void;
+  updated(object?: SohoPopupMenuOptions): void;
 
   /** Tear down the markup for the control */
   teardown(): void;
