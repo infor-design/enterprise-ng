7b078bc71c6e83a4c421ea6a169a6b3985a6809f
Christopher Lepore <chris.lepore@infor.com>
2018-05-09 12:50:28 -0400

    Refer to public ids-enterprise for travis

--- karma.conf.js
+++ karma.conf.js
@@ -29,11 +29,11 @@ module.exports = function (config) {
     },
     files: [
       { pattern: './node_modules/jquery/dist/jquery.js', watched: false  },
-      { pattern: './node_modules/@infor/sohoxi/dist/js/d3.v4.js', watched: false  },
-      { pattern: './node_modules/@infor/sohoxi/dist/js/sohoxi.js', watched: false },
-      { pattern: './node_modules/@infor/sohoxi/dist/js/cultures/en-US.js', watched: false },
+      { pattern: './node_modules/ids-enterprise/dist/js/d3.v4.js', watched: false  },
+      { pattern: './node_modules/ids-enterprise/dist/js/sohoxi.js', watched: false },
+      { pattern: './node_modules/ids-enterprise/dist/js/cultures/en-US.js', watched: false },
       { pattern: './src/test.ts', watched: false },
-      { pattern: './node_modules/@infor/sohoxi/dist/css/light-theme.css', watched: false },
+      { pattern: './node_modules/ids-enterprise/dist/css/light-theme.css', watched: false },
     ],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
