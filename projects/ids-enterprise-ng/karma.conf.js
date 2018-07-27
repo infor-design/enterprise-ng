58d073ee5ea36177fe9d4a334c116c6069f0ad76
Theo Harper <theo.harper@infor.com>
2018-07-27 17:08:44 +0100

    Fixed watch

--- projects/ids-enterprise-ng/karma.conf.js
+++ projects/ids-enterprise-ng/karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function (config) {
     },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
     captureTimeout: 210000,
