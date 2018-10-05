8c477e66a719eb5ac683476c51a3ef478d861cc0
Deepak Verma <deepak.verma@infor.com>
2018-10-05 10:52:07 -0400

    203 - Added support to export custom dataset with Datagrid http://localhost:4200/datagrid-export-without-datagrid

--- projects/ids-enterprise-ng/src/lib/utils/soho-utils.d.ts
+++ projects/ids-enterprise-ng/src/lib/utils/soho-utils.d.ts
@@ -2,3 +2,29 @@ interface JQueryStatic {
   createIcon(options: any): string;
   createIconPath(options: any): string;
 }
+
+interface SohoUtilsExcelStatic {
+  /**
+   * Export the grid contents to csv
+   * @param fileName The desired export filename in the download.
+   * @param customDs An optional customized version of the data to use.
+   * @param self The grid api to use (if customDs is not used)
+   * @returns void
+   */
+  exportToCsv(fileName?: string, customDs?: any, self?: any): void;
+
+  /**
+   * Export the grid contents to xls format. This may give a warning when opening the file.
+   * exportToCsv may be prefered.
+   * @param fileName The desired export filename in the download.
+   * @param worksheetName A name to give the excel worksheet tab.
+   * @param customDs An optional customized version of the data to use.
+   * @param self The grid api if customDS is not used
+   * @returns void
+   */
+  exportToExcel(fileName?: string, worksheetName?: string, customDs?: any, self?: any): void;
+}
+
+interface SohoStatic {
+  excel: SohoUtilsExcelStatic;
+}
