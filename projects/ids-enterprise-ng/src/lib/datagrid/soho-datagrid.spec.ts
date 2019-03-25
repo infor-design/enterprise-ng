b7df7c08f3dd7007061c8bf377e107a10d00a66d
Tim McConechy <tim.mcconechy@infor.com>
2019-03-25 10:13:43 -0400

    Fix the tests because i did an xit on the wrong test.

--- projects/ids-enterprise-ng/src/lib/datagrid/soho-datagrid.spec.ts
+++ projects/ids-enterprise-ng/src/lib/datagrid/soho-datagrid.spec.ts
@@ -574,7 +574,7 @@ describe('Soho DataGrid Render', () => {
     (component.datagrid as any).datagrid.updateCellNode(0, 2, 'Cannondale SuperSix 22', false);
   });
 
-  xit('fires `rowclicked` when a cell clicked', (done) => {
+  it('fires `rowclicked` when a cell clicked', (done) => {
 
     fixture.detectChanges();
 
