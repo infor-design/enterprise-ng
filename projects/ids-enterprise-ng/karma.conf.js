a27abffd3452194eb88e89000f96e418ae131d69
Tim McConechy <tim.mcconechy@infor.com>
2019-03-25 16:21:08 -0400

    Adds more test config and logging

--- projects/ids-enterprise-ng/karma.conf.js
+++ projects/ids-enterprise-ng/karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -60,7 +62,15 @@ module.exports = function (config) {
       },
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--js-flags="--max_old_space_size=4096"']
+        flags: [
+          '--disable-setuid-sandbox',
+          '--no-sandbox', // required to run without privileges in docker
+          '--user-data-dir=/tmp/chrome-test-profile',
+          '--disable-web-security',
+          '--no-proxy-server',
+          '--enable-logging',
+          '--v=1'
+        ]
       }
     },
     port: 9876,
