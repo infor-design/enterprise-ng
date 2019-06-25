e32f0f0e3fc6576291cbae0f790fe4c402f0af5a
pwpatton <phillip.patton@infor.com>
2019-06-25 14:27:21 -0500

    Added new files

---
+++ src/app/calendar/calendar-updated.demo.ts
@@ -0,0 +1,47 @@
+import {
+  ChangeDetectorRef,
+  Component,
+  ViewChild,
+} from '@angular/core';
+import { CalendarDemoService } from './calendar.demo.service';
+import { SohoCalendarComponent } from 'ids-enterprise-ng';
+
+@Component({
+  selector: 'app-calendar-updated-demo',
+  templateUrl: './calendar-updated.demo.html',
+  providers: [CalendarDemoService]
+})
+export class CalendarUpdatedDemoComponent {
+
+  @ViewChild('SohoCalendarComponent') sohoCalendarComponent: SohoCalendarComponent;
+
+  public initialMonth = 1;
+  public initialYear = 2019;
+  public showViewChanger = false;
+  public eventTypes: [];
+  public events: [];
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private monthViewService: CalendarDemoService
+  ) {}
+
+  onRenderMonth(event: SohoCalendarRenderMonthEvent) {
+    console.log('onRenderMonth', event);
+
+    this.monthViewService.getCalendarEventTypes().subscribe((types) => {
+      this.monthViewService.getCalendarEvents().subscribe((events) => {
+        this.eventTypes = types;
+        this.events = events;
+      });
+    });
+  }
+
+  onSelected(event: SohoCalendarDateSelectedEvent) {
+    console.log('onSelected', event);
+  }
+
+  onDblClick(event: MouseEvent) {
+    console.log('onDblClick', event);
+  }
+}
