27eeb60b59c1f50a0636918ec144199c16f7834c
pwpatton <phillip.patton@infor.com>
2019-09-24 15:55:03 -0500

    get basic e2e test working

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,22 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ids-enterprise-ng-app!');
+  it('should display root route header text', async() => {
+    await page.navigateTo();
+    console.log('got here 1');
+
+    expect(await page.getHeaderText()).toContain('IDS Enterprise Angular Components');
+    console.log('got here 2');
+
+    // const headerTextElement = element(by.css('app-header-dynamic-demo soho-header soho-toolbar h1 span'));
+    // expect(headerTextElement).toBeDefined();
+    // expect(headerTextElement.getText()).toContain('IDS Enterprise Angular Components');
+    // console.log('got here');
+
+    // expect(element(by.css('soho-icons'))).toBeDefined();
+    // expect(element(by.css('soho-toolbar'))).toBeDefined();
+    // page.getHeaderText().then(result => console.log('getText result is:', [result]));
+    // console.log('header text is:', );
+    // expect(page.getHeaderText()).toEqual('IDS Enterprise Angular Components');
   });
 });
