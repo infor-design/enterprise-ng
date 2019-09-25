a26b0813e93d092d9638721ce411df4604213bba
Nino Ben Payoyo <nbcpayoyo@gmail.com>
2019-09-25 17:40:38 +0800

    Fix for e2e getText

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,19 +1,19 @@
 import { AppPage } from './app.po';
-import { by, element } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeAll(async() => {
     page = new AppPage();
+    browser.waitForAngularEnabled(false);
   });
 
   it('should display root route header text', async() => {
     await page.navigateTo();
     console.log('got here 1');
 
-    expect(await page.getHeaderText()).toContain('IDS Enterprise Angular Components');
-    console.log('got here 2');
+    expect(page.getHeaderText()).toBe('IDS Enterprise Angular Components');
 
     // const headerTextElement = element(by.css('app-header-dynamic-demo soho-header soho-toolbar h1 span'));
     // expect(headerTextElement).toBeDefined();
