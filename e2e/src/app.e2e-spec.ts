bb9433cda7f5544fcb0cd1b81d92dc275e4c3d75
pwpatton <phillip.patton@infor.com>
2019-09-27 11:12:42 -0500

    simple app e2e spec

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,19 +11,6 @@ describe('workspace-project App', () => {
 
   it('should display root route header text', async() => {
     await page.navigateTo();
-    console.log('got here 1');
-
     expect(page.getHeaderText()).toBe('IDS Enterprise Angular Components');
-
-    // const headerTextElement = element(by.css('app-header-dynamic-demo soho-header soho-toolbar h1 span'));
-    // expect(headerTextElement).toBeDefined();
-    // expect(headerTextElement.getText()).toContain('IDS Enterprise Angular Components');
-    // console.log('got here');
-
-    // expect(element(by.css('soho-icons'))).toBeDefined();
-    // expect(element(by.css('soho-toolbar'))).toBeDefined();
-    // page.getHeaderText().then(result => console.log('getText result is:', [result]));
-    // console.log('header text is:', );
-    // expect(page.getHeaderText()).toEqual('IDS Enterprise Angular Components');
   });
 });
