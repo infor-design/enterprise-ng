1a26a9ffa4fda911bc6f6e5c446b6fcbfeca27f9
Nino Ben Payoyo <nbcpayoyo@gmail.com>
2019-11-12 15:03:09 +0800

    fixed datagrid contextmenu tests

--- cypress/integration/datagrid/datagrid-dynamic.spec.js
+++ cypress/integration/datagrid/datagrid-dynamic.spec.js
@@ -1,25 +1,41 @@
 context('Datagrid Dynamic Tests', () => {
-  beforeEach(() => {
+
+  before(() => {
     cy.visit('/datagrid-dynamic');
-  })
+    // close side nav to maximize viewport for the main content
+    cy.get('.header > .toolbar > .title > .btn-icon').click();
+    // ensure datagrid is ready with busy indicator already hidden
+    cy.get('table.datagrid tr').should('exist');
+    cy.get('.overlay.busy').should('not.exist');
+  });
 
-  it('should show context menu', () => {
-    cy.contains('Compressor').rightclick();
-    cy.get('div.popupmenu-wrapper ul')
-      .should('be.visible')
-      .children()
-      .should('have.length', 5);
+  // close the context menu
+  afterEach(() => {
+    cy.get('ul[soho-popupmenu] li:first>a').click();
+    cy.get('ul[soho-popupmenu]').should('not.be.visible');
+  });
 
-    cy.contains('2142201 Compressor').rightclick();
-    cy.get('div.popupmenu-wrapper ul')
-      .should('be.visible')
-      .children()
-      .should('have.length', 10);
+  it('should show 1st context menu', () => {
+    // rightclick is still buggy
+    cy.get('table.datagrid tr:first').trigger('contextmenu')
+      .should('have.class', 'is-selected');
+    cy.get('ul[soho-popupmenu]').should('be.visible')
+      .children().should('have.length', 5);
+  });
 
-    cy.contains('2142201 Compressor').rightclick();
-    cy.get('div.popupmenu-wrapper ul')
-      .should('be.visible')
-      .children()
-      .should('have.length', 3);
+  it('should show 2nd context menu', () => {
+    // rightclick is still buggy
+    cy.get('table.datagrid tr:nth-child(3)').trigger('contextmenu')
+      .should('have.class', 'is-selected');
+    cy.get('ul[soho-popupmenu]').should('be.visible')
+      .children().should('have.length', 10);
+  });
+
+  it('should show 3rd context menu', () => {
+    // rightclick is still buggy
+    cy.get('table.datagrid tr:last').trigger('contextmenu')
+      .should('have.class', 'is-selected');
+    cy.get('ul[soho-popupmenu]').should('be.visible')
+      .children().should('have.length', 3);
   });
 });
