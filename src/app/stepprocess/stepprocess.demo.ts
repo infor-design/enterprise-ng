6dcdc37ce9de302f97dfc5340f33242e7fa2abf4
JBismonte <judebismonte@gmail.com>
2020-04-07 17:56:54 +0800

    #772 enhanced code change to use angular instead of jquery
    I have also included modified changes in the stepprocess.demo.html to preview the changes.

--- src/app/stepprocess/stepprocess.demo.ts
+++ src/app/stepprocess/stepprocess.demo.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { SohoStepProcessComponent } from 'ids-enterprise-ng';
 @Component({
   selector: 'app-stepprocess-demo',
   templateUrl: 'stepprocess.demo.html'
 })
 export class StepProcessDemoComponent {
+  nextLabel = "NEXT";
+  previousLabel = "PREVIOUS";
+
   onSaveClose(event: SohoStepSaveCloseEvent) {
     console.log('onSaveClose fired');
     console.log('currentStepId: ' + event.currentStepId);
