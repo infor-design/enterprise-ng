c3f9965d3312c5813a6d12ddb77e9312059a2a50
Tim McConechy <tim.mcconechy@infor.com>
2020-05-22 15:00:43 -0400

    Fix lint errors

--- src/app/popdown/popdown.demo.ts
+++ src/app/popdown/popdown.demo.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ChangeDetectorRef, ChangeDetectionStrategy, } from '@angular/core';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
 
 import {
   SohoPopDownDirective,
@@ -20,7 +20,6 @@ export class PopDownDemoComponent {
   @ViewChild(SohoListViewComponent, { static: true }) public stateViewList: SohoListViewComponent;
 
   public showSelectedOnly = false;
-
   public columns_product: SohoDataGridColumn[];
   public data_product: any[];
   public model: any = {
@@ -36,23 +35,6 @@ export class PopDownDemoComponent {
       action: 'Action',
     }
   };
-
-  setupProducts() {
-    this.columns_product = [];
-    this.data_product = [];
-
-    // Some Sample Data
-    productsData.forEach(data => {
-      this.data_product.push(data);
-    });
-
-    // Define Columns for the Grid.
-    productsColumns.forEach(column => {
-      this.columns_product.push(column);
-    });
-
-  }
-
   public states = [
     'Alabama',
     'Alaska',
@@ -105,21 +87,35 @@ export class PopDownDemoComponent {
     'Wisconsin',
     'Wyoming'
   ];
-
   public selectedStates = [];
+
+  setupProducts() {
+    this.columns_product = [];
+    this.data_product = [];
+
+    // Some Sample Data
+    productsData.forEach(data => {
+      this.data_product.push(data);
+    });
+
+    // Define Columns for the Grid.
+    productsColumns.forEach(column => {
+      this.columns_product.push(column);
+    });
+
+  }
 
   public get visibleStates() {
     return this.showSelectedOnly ? this.selectedStates : this.states;
   }
 
   constructor(protected changeDetector: ChangeDetectorRef) {
-
   }
 
-  public onSelected(event) {
+  public onSelected(event: any) {
     this.selectedStates = [];
     if (event.length > 1 && event[1].selectedItems !== undefined) {
-      event[1].selectedItems.forEach((item) => {
+      event[1].selectedItems.forEach((item: any) => {
         const data = $(item).attr('data');
         console.log(data);
         this.selectedStates.push(data);
@@ -128,11 +124,11 @@ export class PopDownDemoComponent {
     }
   }
 
-  public onClickOK(event: any) {
+  public onClickOK() {
     this.statesPopDown.close();
   }
 
-  public onClickCancel(event: any) {
+  public onClickCancel() {
     this.statesPopDown.close();
   }
 }
