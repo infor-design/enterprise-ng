29720a78ec8cd405f3fcbcfc960119060994accd
Tim McConechy <tim.mcconechy@infor.com>
2020-08-24 10:06:55 -0400

    Update the example back - this isnt working

--- src/app/modal-dialog/modal-dialog.demo.ts
+++ src/app/modal-dialog/modal-dialog.demo.ts
@@ -130,11 +130,11 @@ export class ModalDialogDemoComponent {
   }
 
   openDialogResult() {
-    this.modalService
+    const dialogRef = this.modalService
       .message('<span class="message">Are you sure you want to delete this page?</span>')
       .buttons(
-        [{ text: 'YES', click: (_e, modal) => { modal.close('YES'); } },
-        { text: 'NO', click: (_e, modal) => { modal.close('NO'); }, isDefault: true }])
+        [{ text: 'YES', click: () => { dialogRef.close('YES'); } },
+        { text: 'NO', click: () => { dialogRef.close('NO'); }, isDefault: true }])
       .title(this.title)
       .open()
       .afterClose(result => {
