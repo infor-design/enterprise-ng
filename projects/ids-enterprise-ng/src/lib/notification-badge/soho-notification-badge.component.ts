0614c532ef0b5516f10d4b03b96ba8cd7afc5c9e
Eric Angeles <enrico.angeles@infor.com>
2021-07-26 18:31:39 +0800

    add remaining settings and add test page

--- projects/ids-enterprise-ng/src/lib/notification-badge/soho-notification-badge.component.ts
+++ projects/ids-enterprise-ng/src/lib/notification-badge/soho-notification-badge.component.ts
@@ -2,7 +2,6 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
-  HostBinding,
   Input,
   NgZone,
   OnInit,
@@ -21,9 +20,8 @@ export class SohoNotificationBadgeComponent implements OnInit {
   constructor(
     private element: ElementRef,
     private ngZone: NgZone,
-  ) {
+  ) { }
 
-  }
   @Input() set position(position: string | undefined) {
     (this.options as any).position = position;
     if (this.notificationBadge) {
@@ -38,20 +36,62 @@ export class SohoNotificationBadgeComponent implements OnInit {
     return (this.options as any).position;
   }
 
+  @Input() set icon(icon: string | undefined) {
+    (this.options as any).icon = icon;
+    if (this.notificationBadge) {
+      this.notificationBadge.settings.icon = icon;
+    }
+  }
+  get icon(): string | undefined {
+    if (this.notificationBadge) {
+      return this.notificationBadge.settings.icon;
+    }
+
+    return (this.options as any).icon;
+  }
+
+  @Input() set color(color: string | undefined) {
+    (this.options as any).color = color;
+    if (this.notificationBadge) {
+      this.notificationBadge.settings.color = color;
+    }
+  }
+  get color(): string | undefined {
+    if (this.notificationBadge) {
+      return this.notificationBadge.settings.color;
+    }
+
+    return (this.options as any).color;
+  }
+
+  @Input() set attributes(attributes: Array<Object> | Object | undefined) {
+    (this.options as any).attributes = attributes;
+    if (this.notificationBadge) {
+      this.notificationBadge.settings.attributes = attributes;
+    }
+  }
+  get attributes(): Array<Object> | Object | undefined {
+    if (this.notificationBadge) {
+      return this.notificationBadge.settings.attributes;
+    }
+
+    return (this.options as any).attributes;
+  }
+
   ngOnInit() {
     this.ngZone.runOutsideAngular(() => {
-
       this.jQueryElement = jQuery(this.element.nativeElement);
 
       const options: SohoNotificationBadgeOptions = {
         position: this.position,
+        icon: this.icon,
+        color: this.color,
+        attributes: this.attributes
       };
 
+      // Initialize the notification badge component.
       this.jQueryElement.notificationbadge(options);
-      console.log({
-        jq: this.jQueryElement,
-        el: this.element
-      });
+
     });
   }
 }
--- src/app/notification-badge/notification-badge-placement.demo.ts
+++ src/app/notification-badge/notification-badge-placement.demo.ts
@@ -1,26 +1,14 @@
 import {
   Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
 } from '@angular/core';
-
-import { SohoNotificationBadgeComponent } from 'ids-enterprise-ng';
 
 @Component({
   selector: 'app-notification-badge-placement-demo',
-  templateUrl: 'notification-badge-placement.demo.html'
+  templateUrl: 'notification-badge-placement.demo.html',
+  styles: [`
+  .container-spacer {flex-grow: 1}
+  `]
 })
-export class NotificationBadgePlacementDemoComponent implements OnInit {
-  @ViewChild(SohoNotificationBadgeComponent) sohoNotificationBadgeComponent?: SohoNotificationBadgeComponent;
+export class NotificationBadgePlacementDemoComponent {
 
-  constructor(
-    private element: ElementRef,
-  ) {
-
-  }
-
-  ngOnInit(): void {
-    console.log({ this: this, el: this.element.nativeElement });
-  }
 }
