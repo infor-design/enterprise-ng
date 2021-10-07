dbbd66bf22859d03a86392c9665fcec7fff9b352
Yohannah Bautista <ybautista@PHMAMYBAUTIST01.local>
2021-10-07 10:15:18 +0800

    5562. Added hide in notification service

--- projects/ids-enterprise-ng/src/lib/notification/soho-notification.service.ts
+++ projects/ids-enterprise-ng/src/lib/notification/soho-notification.service.ts
@@ -7,10 +7,34 @@ export class SohoNotificationService {
   public static INFO: SohoNotificationType = 'info';
   public static SUCCESS: SohoNotificationType = 'success';
 
+  private notifApis: Array<JQuery.PlainObject> = [];
+
   /**
    * Show a notification using the specified options.
    */
   show(options: SohoNotificationOptions): void {
-    jQuery('body').notification(options);
+    this.notifApis.push(jQuery('body').notification(options).data('notification'));
+  }
+
+  /**
+   * Hide a specified notification
+   * @param id id of notification
+   */
+  hide(id: string): void {
+    // find api in array, if exist then close
+    if (this.notifApis.length > 0) {
+      const index = this.notifApis.findIndex((notif) => {
+        if (Array.isArray(notif.settings.attributes)) {
+          return notif.settings.attributes.some((attribute: any) => attribute.name === 'id' && attribute.value === id);
+        } else {
+          return notif.settings.attributes.name === 'id' && notif.settings.attributes.value === id;
+        }
+      });
+
+      if (index >= 0) {
+        this.notifApis[index].close();
+        this.notifApis.splice(index, 1);
+      }
+    }
   }
 }
