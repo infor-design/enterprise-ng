1ecb0f6b17b5cba75c1d53937527a279a0fe3d5c
James Allen <jamesallen.tadique@infor.com>
2021-11-24 23:31:19 +0800

    1169. Buttons

--- src/app/button/button.demo.ts
+++ src/app/button/button.demo.ts
@@ -11,12 +11,28 @@ import {
 })
 export class ButtonDemoComponent implements OnInit {
   public shouldSayHi = false;
-
-  public notifOptions = {
+  
+  public badgeOption1: SohoButtonOptions = {
     badge: true,
     badgeOptions: {
       position: 'upper-left',
+      color: 'alert'
+    }
+  }
+
+  public badgeOption2: SohoButtonOptions = {
+    badge: true,
+    badgeOptions: {
+      position: 'lower-left',
       color: 'yield'
+    }
+  }
+
+  public badgeOption3: SohoButtonOptions = {
+    badge: true,
+    badgeOptions: {
+      position: 'lower-right',
+      color: 'complete'
     }
   }
   
