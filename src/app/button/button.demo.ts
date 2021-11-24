ad1a61ffa1fb6c999e76c87aaa7677dc748d3f52
James Allen <jamesallen.tadique@infor.com>
2021-11-24 14:37:41 +0800

    1169. Buttons

--- src/app/button/button.demo.ts
+++ src/app/button/button.demo.ts
@@ -12,6 +12,14 @@ import {
 export class ButtonDemoComponent implements OnInit {
   public shouldSayHi = false;
 
+  public notifOptions = {
+    badge: true,
+    badgeOptions: {
+      position: 'upper-left',
+      color: 'yield'
+    }
+  }
+  
   constructor() {}
   ngOnInit() {}
   toggleHello() {
