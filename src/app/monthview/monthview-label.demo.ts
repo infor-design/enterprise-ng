a929ad2dd3e5f2ba04c66750fed64ad25e4c31e8
Tim McConechy <tim.mcconechy@infor.com>
2022-06-02 10:50:32 -0400

    Add ajax example

--- src/app/monthview/monthview-label.demo.ts
+++ src/app/monthview/monthview-label.demo.ts
@@ -1,42 +1,54 @@
 import {
-    Component,
-    ViewChild
-  } from '@angular/core';
-  
+  Component,
+  ViewChild
+} from '@angular/core';
+
 import { SohoMonthViewComponent } from 'ids-enterprise-ng';
 import { BehaviorSubject, Observable } from 'rxjs';
-  
+
 @Component({
   selector: 'app-monthview-label-demo',
   templateUrl: 'monthview-label.demo.html',
 })
 export class MonthViewLabelDemoComponent {
-    @ViewChild(SohoMonthViewComponent) sohoMonthViewComponent?: SohoMonthViewComponent;
+  @ViewChild(SohoMonthViewComponent) sohoMonthViewComponent?: SohoMonthViewComponent;
 
-    private _legendDataSubject = new BehaviorSubject<SohoMonthViewLegend[]>([
-        { name: 'Warnings', color: 'amber07', dates: [], dayOfWeek: [] },
-        { name: 'Scheduled', color: 'azure03', dates: ['1/7/2017'], dayOfWeek: [] },
+  private _legendDataSubject = new BehaviorSubject<SohoMonthViewLegend[]>([
+    { name: 'Warnings', color: 'amber07', dates: [], dayOfWeek: [] },
+    { name: 'Scheduled', color: 'azure03', dates: ['1/7/2017'], dayOfWeek: [] },
+  ]);
+
+  public readonly legendData$: Observable<SohoMonthViewLegend[]> = this._legendDataSubject.asObservable();
+
+  public readonly monthViewOptions: SohoMonthViewOptions = {
+    day: 5,
+    month: 0,
+    year: 2017,
+    inPage: true,
+    inPageExpanded: true,
+    showMonthYearPicker: false,
+    showLegend: true,
+  };
+
+  public cnt = 0;
+
+  onRenderMonth(event: SohoMonthViewRenderEvent) {
+    // Fake Ajax Call
+    setTimeout(() => {
+      this.updateLegend();
+    }, 250);
+    console.log('onRenderMonth', event);
+  }
+
+  public updateLegend(): void {
+    this.cnt++;
+    console.log('updateLegend', event);
+    this._legendDataSubject.next([
+      { name: 'Warnings ' + this.cnt, color: 'amber07', dates: ['1/10/2017'], dayOfWeek: [] },
+      { name: 'Scheduled', color: 'azure03', dates: [], dayOfWeek: [] },
+      { name: 'Public Holiday', color: 'emerald06', dates: ['1/1/2017', '1/2/2017', '1/12/2017'], dayOfWeek: [] },
+      { name: 'Other', color: 'ruby03', dates: ['1/8/2017', '1/9/2017', '1/23/2017'], dayOfWeek: [] },
+      { name: 'Half Days', color: 'amethyst06', dates: ['1/21/2017', '1/22/2017'], dayOfWeek: [] },
     ]);
-
-    public readonly legendData$: Observable<SohoMonthViewLegend[]> = this._legendDataSubject.asObservable(); 
-
-    public readonly monthViewOptions: SohoMonthViewOptions = {
-        day: 5,
-        month: 0,
-        year: 2017,
-        inPage: true,
-        inPageExpanded: true,
-        showMonthYearPicker: false,
-        showLegend: true,
-    };  
-
-    public updateLegend(): void {
-      this._legendDataSubject.next([
-        { name: 'Warnings', color: 'amber07', dates: ['1/10/2017'], dayOfWeek: [] },
-        { name: 'Scheduled', color: 'azure03', dates: [], dayOfWeek: [] },
-        { name: 'Public Holiday', color: 'emerald06', dates: ['1/1/2017', '1/2/2017', '1/12/2017'], dayOfWeek: [] },
-        { name: 'Other', color: 'ruby03', dates: ['1/8/2017', '1/9/2017', '1/23/2017'], dayOfWeek: [] },
-        { name: 'Half Days', color: 'amethyst06', dates: ['1/21/2017', '1/22/2017'], dayOfWeek: [] },
-      ]);
-    }
+  }
 }
