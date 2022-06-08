2357e99d1fd2ef5080938dbfca4bb8067949826f
Yohannah Bautista <yohannahbautista@gmail.com>
2022-06-08 16:41:19 +0800

    1305. Updated test page

--- src/app/monthview/monthview-label.demo.ts
+++ src/app/monthview/monthview-label.demo.ts
@@ -31,24 +31,27 @@ export class MonthViewLabelDemoComponent {
 
   onRenderMonth(event: SohoMonthViewRenderEvent) {
     console.log('onRenderMonth', event);
-    // Fake Ajax Call
-    setTimeout(() => {
-      console.log('Fake ajax call')
-      this._legendDataSubject.next([
-        { name: `Warnings ${this.cnt}`, color: 'amber07', dates: [], dayOfWeek: [] },
-        { name: `Scheduled ${this.cnt}`, color: 'azure03', dates: ['1/7/2017'], dayOfWeek: [] },
-      ]);
-    }, 250);
+    this.loadLegend([
+      { name: `Warnings ${this.cnt}`, color: 'amber07', dates: [], dayOfWeek: [] },
+      { name: `Scheduled ${this.cnt}`, color: 'azure03', dates: ['1/7/2017'], dayOfWeek: [] },
+    ]);
   }
 
   public updateLegend(): void {
     this.cnt++;
-    this._legendDataSubject.next([
+    this.loadLegend([
       { name: `Warnings ${this.cnt}`, color: 'amber07', dates: ['1/10/2017'], dayOfWeek: [] },
       { name: `Scheduled ${this.cnt}`, color: 'azure03', dates: [], dayOfWeek: [3, 5] },
       { name: `Public Holiday ${this.cnt}`, color: 'emerald06', dates: ['1/1/2017', '1/2/2017', '1/12/2017'], dayOfWeek: [] },
       { name: `Other ${this.cnt}`, color: 'ruby03', dates: ['1/8/2017', '1/9/2017', '1/23/2017'], dayOfWeek: [] },
       { name: `Half Days ${this.cnt}`, color: 'amethyst06', dates: ['1/21/2017', '1/22/2017'], dayOfWeek: [] },
     ]);
+  }
+
+  private loadLegend(legendList: SohoMonthViewLegend[]) {
+    setTimeout(() => {
+      this._legendDataSubject.next(legendList);
+      console.log('Fake ajax call', this._legendDataSubject.value);
+    }, 250);
   }
 }
