936d30183519e92fdefd4abc01433c306d850abc
Tim McConechy <tim.mcconechy@infor.com>
2023-08-29 15:12:47 -0400

    Fix security scan issues

--- src/app/dropdown/dropdown-mock.data.ts
+++ src/app/dropdown/dropdown-mock.data.ts
@@ -8,6 +8,4 @@ export const MOCK_STATES = [
   { value: 'OR', label: 'Oregon' },
   { value: 'WA', label: 'Washington' },
   { value: 'WY', label: 'Wyoming' },
-  { value: '<script>window.alert("dropdown xss")</script>XSS' , label: '<script>window.alert("dropdown xss")</script>XSS' },
-  { value: '<script>window.alert(\'xss\'); debugger;</script>XSS' , label: '<script>window.alert("dropdown xss")debugger;</script>XSS' },
 ];
