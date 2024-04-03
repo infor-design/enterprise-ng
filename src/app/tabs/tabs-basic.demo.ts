688d3906e39e7024e988e14d3514f479b0c9adb8
Yohannah Bautista <yohannahbautista@gmail.com>
2024-04-03 15:32:24 +0800

    1647. Removed unused import

--- src/app/tabs/tabs-basic.demo.ts
+++ src/app/tabs/tabs-basic.demo.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { SohoTabsComponent, SohoToastService } from 'ids-enterprise-ng';
 
 /**
