833d82922c412dcec7d5a6adf5b85e3602909fc8
Tim McConechy <tim.mcconechy@infor.com>
2024-09-13 09:47:06 -0400

    Make new release

--- projects/ids-enterprise-ng/src/lib/version/version-initializer.service.ts
+++ projects/ids-enterprise-ng/src/lib/version/version-initializer.service.ts
@@ -15,7 +15,7 @@ export class SohoVersionInitializerService {
    * Initializes the version attribute.
    */
   initialize() {
-    $('html').attr(SohoVersionInitializerService.VERSION_ATTR_NAME, '18.3.0');
+    $('html').attr(SohoVersionInitializerService.VERSION_ATTR_NAME, '18.3.3');
   }
 }
 
