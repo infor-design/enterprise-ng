6470de137225e2954845940c89c8a9576b52af84
Tim McConechy <tim.mcconechy@infor.com>
2024-11-08 13:08:37 -0500

    Bump version

--- projects/ids-enterprise-ng/src/lib/version/version-initializer.service.ts
+++ projects/ids-enterprise-ng/src/lib/version/version-initializer.service.ts
@@ -15,7 +15,7 @@ export class SohoVersionInitializerService {
    * Initializes the version attribute.
    */
   initialize() {
-    $('html').attr(SohoVersionInitializerService.VERSION_ATTR_NAME, '18.7.0');
+    $('html').attr(SohoVersionInitializerService.VERSION_ATTR_NAME, '18.8.0-dev');
   }
 }
 
